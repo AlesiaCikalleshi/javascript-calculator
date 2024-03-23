10c331254b1cd7cc0ba01a6658b70c9ff1683004
Alesia_Cikalleshi <alesiacikalleshi@gmail.com>
2024-03-23 14:00:27 +0100

    Fixing display function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,35 @@
-function clearScreen(){
+function clearScreen() {
     document.getElementById('result').value = "";
 }
 
 
+let operators = ['+', '-', '*', '/'];
+
 function display(value) {
-    document.getElementById("result").value += value;
+    const resultInput = document.getElementById("result");
+    const currentValue = resultInput.value;
+
+    if (value === '') {
+        // If value is empty, allow only numbers
+        if (!isNaN(parseInt(currentValue.charAt(currentValue.length - 1)))) {
+            resultInput.value += value;
+        }
+        return;
+    } else if (operators.includes(value)) {
+        // The charAt() method returns the character at the specified index within the string
+        if (currentValue !== '' && operators.includes(currentValue.charAt(currentValue.length - 1))) {
+            // If the last entered character is an operator, replace it
+            resultInput.value = currentValue.slice(0, -1) + value;
+        } else {
+            resultInput.value += value;
+        }
+        return;
+    }
+    resultInput.value += value;
 }
- 
 
 
-function calculateResult(){
+function calculateResult() {
     const x = document.getElementById('result').value;
     const y = eval(x)
     document.getElementById('result').value = y;
